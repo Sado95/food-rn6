1b84585e338778b189821131623866f44217e7f6
Sado95 <elsadet.dervisevic95@gmail.com>
2023-08-30 14:04:41 +0200

    Update

---
+++ src/components/SearchBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather style={styles.iconStyle} name="search" size={30} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: "#f0eeee",
+    borderRadius: 5,
+    height: 50,
+    marginHorizontal: 15,
+    marginTop: 20,
+    display: "flex",
+    flexDirection: "row",
+    marginBottom: 10,
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
+  },
+});
+
+export default SearchBar;
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { View, StyleSheet, Text, ScrollView } from "react-native";
+import useResults from "../hooks/useResults";
+
+/* COMPONENT IMPORT */
+
+import SearchBar from "../components/SearchBar";
+import ResultsList from "../components/ResultsList";
+
+const SearchScreen = ({ navigation }) => {
+  const [term, setTerm] = useState("");
+  const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = (price) => {
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
+
+  return (
+    <View style={styles.background}>
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <ScrollView>
+        <ResultsList
+          title="Cost Effective"
+          results={filterResultsByPrice("$")}
+        />
+
+        <ResultsList title="Bit Pricier" results={filterResultsByPrice("$$")} />
+
+        <ResultsList
+          title="Big Spender"
+          results={filterResultsByPrice("$$$")}
+        />
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#fff",
+    flex: 1,
+  },
+});
+
+export default SearchScreen;
